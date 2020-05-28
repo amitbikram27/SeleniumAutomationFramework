f09a4fc69a385fd840e65f78976486a379e7edb4
amitbikram27 <amit.bikram27@gmail.com>
2020-05-28 21:02:24 +0530

    My First Commit Prog

---
+++ Automation_Framework/src/main/java/com/Amit/Selenium/App.java
@@ -0,0 +1,13 @@
+package com.Amit.Selenium;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
